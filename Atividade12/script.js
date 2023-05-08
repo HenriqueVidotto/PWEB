ab67524bc0ac72b61425b7d04e503067f3c1fd03
HenriqueVidotto <99729533+HenriqueVidotto@users.noreply.github.com>
2023-05-08 11:59:13 -0300

    Add files via upload

---
+++ Atividade12/script.js
@@ -0,0 +1,21 @@
+function alterar() {
+
+    var imagem = document.getElementById("teste");
+    var tex = document.getElementById("texto");
+    imagem.src = "https://static.vecteezy.com/ti/vetor-gratis/p3/1750458-janela-aberta-de-madeira-com-venezianas-e-vidro-transparente-vetor.jpg";
+    tex.innerHTML = " Janela Aberta"
+}
+
+function alterar2(){
+    var imagem = document.getElementById("teste");
+    var tex = document.getElementById("texto");
+    imagem.src = "https://static.vecteezy.com/ti/vetor-gratis/p3/1750481-veneziana-fechada-janela-madeira-com-floreira-vetor.jpg";
+    tex.innerHTML = " Janela Fechada"
+}
+function alterar3(){
+    var imagem = document.getElementById("teste");
+    var tex = document.getElementById("texto");
+    
+    imagem.src = "https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/251565/159001_101932.jpeg";
+    tex.innerHTML = " Janela Quebrada"
+}
